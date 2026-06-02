246aaa2bf937245c39d542110f25d9b276e7a2c1
luizenata <luiz308araujofarias@gmail.com>
2026-06-02 09:24:13 -0300

    Criando a pagina de colaborador(foi o carlos)

---
+++ App_Login/Areas/Colaborador/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using App_Login.Libraries.Login;
+using App_Login.Repositories.Contract;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Razor.Language.Intermediate;
+
+namespace App_Login.Areas.Colaborador.Controllers
+{
+    [Area("Colaborador")]
+    public class HomeController : Controller
+    {
+
+        private readonly IColaboradorRepository _colaboradorRepository;
+        private readonly LoginColaborador _loginColaborador;
+
+        public HomeController(IColaboradorRepository repository, LoginColaborador loginCliente)
+        {
+            _colaboradorRepository = repository;
+            _loginColaborador = loginCliente;
+        }
+        public IActionResult Login()
+        {
+            return View();
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult PainelGerente()
+        {
+            return View();
+        }
+    }
+}
