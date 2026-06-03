8e36f6a0df741da2642329083977faf068558a84
luizenata <natabarsottiplaca@gmail.com>
2026-06-03 12:10:46 -0300

    Criando area colaborador

--- App_Login/Areas/Colaborador/Controllers/HomeController.cs
+++ App_Login/Areas/Colaborador/Controllers/HomeController.cs
@@ -1,25 +1,51 @@
 ﻿using App_Login.Libraries.Login;
+using App_Login.Models.Constant;
 using App_Login.Repositories.Contract;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Razor.Language.Intermediate;
 
-namespace App_Login.Areas.Colaborador.Controllers
+namespace AppLogin.Areas.Colaborador.Controllers
 {
     [Area("Colaborador")]
     public class HomeController : Controller
     {
-
-        private readonly IColaboradorRepository _colaboradorRepository;
-        private readonly LoginColaborador _loginColaborador;
-
-        public HomeController(IColaboradorRepository repository, LoginColaborador loginCliente)
+        private IColaboradorRepository _repositoryColaborador;
+        private LoginColaborador _loginColaborador;
+        public HomeController(IColaboradorRepository repositoryColaborador, LoginColaborador loginColaborador)
         {
-            _colaboradorRepository = repository;
-            _loginColaborador = loginCliente;
+            _repositoryColaborador = repositoryColaborador;
+            _loginColaborador = loginColaborador;
         }
         public IActionResult Login()
         {
             return View();
+        }
+        [HttpPost]
+        public IActionResult Login([FromForm] App_Login.Models.Colaborador colaborador)
+        {
+            App_Login.Models.Colaborador colaboradorDB = _repositoryColaborador.Login(colaborador.Email, colaborador.Senha);
+
+            if (colaboradorDB.Email != null &&
+                colaboradorDB.Senha != null &&
+                colaboradorDB.Tipo != ColaboradorTipoConstant.Comum)
+            {
+                _loginColaborador.Login(colaboradorDB);
+
+                return new RedirectResult(Url.Action(nameof(PainelGerente)));
+            }
+
+            if (colaboradorDB.Email != null &&
+                colaboradorDB.Senha != null &&
+                colaboradorDB.Tipo != ColaboradorTipoConstant.Gerente)
+            {
+                _loginColaborador.Login(colaboradorDB);
+
+                return new RedirectResult(Url.Action(nameof(PainelComum)));
+            }
+            else
+            {
+                ViewData["MSG_E"] = "Usuário não encontrado, verifique o e-mail e senha digitado!";
+                return View();
+            }
         }
         public IActionResult Index()
         {
@@ -27,7 +53,27 @@ namespace App_Login.Areas.Colaborador.Controllers
         }
         public IActionResult PainelGerente()
         {
+            ViewBag.Nome = _loginColaborador.GetColaborador().Nome;
+            ViewBag.Tipo = _loginColaborador.GetColaborador().Tipo;
+            ViewBag.Email = _loginColaborador.GetColaborador().Email; 
             return View();
+        }
+        public IActionResult PainelComum()
+        {
+            ViewBag.Nome = _loginColaborador.GetColaborador().Nome;
+            ViewBag.Tipo = _loginColaborador.GetColaborador().Tipo;
+            ViewBag.Email = _loginColaborador.GetColaborador().Email;
+            return View();
+        }
+
+        public IActionResult Painel()
+        {
+           return View();
+        }
+        public IActionResult Logout()
+        {
+            _loginColaborador.Logout();
+            return RedirectToAction("Login", "Home");
         }
     }
 }
--- App_Login/obj/Debug/net10.0/App_Login.AssemblyInfo.cs
+++ App_Login/obj/Debug/net10.0/App_Login.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("App_Login")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+869e9a7cbbcc5177f13c94ed7f45ce3204bd68a1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+246aaa2bf937245c39d542110f25d9b276e7a2c1")]
 [assembly: System.Reflection.AssemblyProductAttribute("App_Login")]
 [assembly: System.Reflection.AssemblyTitleAttribute("App_Login")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
